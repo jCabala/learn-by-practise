1a71f5f723ad3950d98ca9c594cf8fdb950a2acf
Jakub Cabala <kuba.cabala6@gmail.com>
2024-12-24 13:10:13 +0100

    competetiveProgramming/przygodyBajtazara: prz

--- competetiveProgramming/przygodyBajtazara/Part1/AmortisedCost/prz.cpp
+++ competetiveProgramming/przygodyBajtazara/Part1/AmortisedCost/prz.cpp
@@ -1,195 +1,95 @@
-//https://szkopul.edu.pl/problemset/problem/bobLSP2Wo3SQjakifBIQhXlq/site/?key=statement
+// https://szkopul.edu.pl/problemset/problem/bobLSP2Wo3SQjakifBIQhXlq/site/?key=statement
 #include <bits/stdc++.h>
 using namespace std;
 
-// Debug
-const bool DEBUG = false;
+const int MAX_N = 1000001;
+const int MAX_K = 1000001;
 
-// Data
-int n, k, l, r;
-vector<int> houses, leftBandit, rightBandit; 
+int N, K, meetHouseColor;
+int houses[MAX_N], nextPtr[MAX_N], sequence[MAX_N];
+int banditPosition[MAX_K], banditColor[MAX_N];
+int rangeEndingForIdx[2][MAX_N], result[2][MAX_K];
 
-// Tracking sequences
-int meetColor;
-map<int, vector<int>> colorToIndexes;
-vector<int> leftBanditIdxIndexes, rightBanditIdxIndexes;
-
-// Tracking same color houses
-int leftMostIdx, rightMostIdx, countLeftAndRight = 0;
-map<int, int> leftColorToCount, rightColorToCount;
-
-bool cinBandit(vector<int> &bandit, vector<int>&indexes, int size) {
-    for (int i = 0; i < size; i++) {
-        int house;
-        cin >> house;
-        bandit.push_back(house);
-        indexes.push_back(0);
-    }
-    
-    return true;
-}
-
-bool cinData() {
-    cin >> n >> k;
-
-    // Cin houses
-    for (int i = 0; i < n; i++) {
-        int house;
-        cin >> house;
-        houses.push_back(house);
-
-        colorToIndexes[houses[i]].push_back(i);
+void setupBandit(int banditSize, int banditNum) {
+    // Creating a linked list of pointers to the next house of the same color
+    fill(banditPosition, banditPosition+K, N);
+    for (int j = N-1; j >= 0; j--) {
+        nextPtr[j] = banditPosition[houses[j]];
+        banditPosition[houses[j]] = j;
     }
 
-    // cin bandits
-    cin >> l >> r;
-    bool success = cinBandit(leftBandit, leftBanditIdxIndexes, l);
-    if (!success) return false;
-    success = cinBandit(rightBandit, rightBanditIdxIndexes, r);
-    if (!success) return false;
+    // Reading the colors of the houses for the ith rober
+    for (int j = 1; j <= banditSize; j++) {
+        cin >> banditColor[j];
+        banditColor[j]--;
+    }
+    meetHouseColor = banditColor[banditSize]; // Used later in main
 
-    meetColor = rightBandit[rightBandit.size() - 1];
-    rightBandit.pop_back(); leftBandit.pop_back();
-
-    // intitiate indexes
-    leftMostIdx = colorToIndexes[leftBandit[0]][0];
-    rightMostIdx = colorToIndexes[rightBandit[0]][0];
-    
-    // Initiate counts
-    for (int i = 0; i < leftMostIdx; i++) {
-        leftColorToCount[houses[i]]++;
+    // Creating the initial sequence for the first color
+    // Iterate for each color and setup "left most" sequence
+    int curPos = 0;
+    for (int j = 1; j <= banditSize; j++) {
+        while (banditPosition[banditColor[j]] < curPos)
+            banditPosition[banditColor[j]] = nextPtr[banditPosition[banditColor[j]]];
+        curPos = banditPosition[banditColor[j]];
+        sequence[j] = curPos;
     }
 
-    for (int i = rightMostIdx + 1; i < houses.size(); i++) {
-        rightColorToCount[houses[i]]++;
-        if (leftColorToCount[houses[i]] > 0 && rightColorToCount[houses[i]] == 1) {
-            countLeftAndRight++;
-        }
-    }
-    return true;
-}
+    for (int j = 0; j < N; j++) {
+        int curPos = 0;
+        sequence[0] = j;
 
-void updateCounts(int newLeftMostIdx, int newRightMostIdx) {
-    // Update left
-    for (int i = leftMostIdx; i <= newLeftMostIdx; i++) {
-        leftColorToCount[houses[i]]++;
-        if (leftColorToCount[houses[i]] == 1 && rightColorToCount[houses[i]] > 0) {
-            countLeftAndRight++;
-        }
-    }
-    leftMostIdx = newLeftMostIdx;
-    // Update right
-    // ERROR HERE !!!!!!!!!!!!!!!!!!!!!!!
-    for (int i = rightMostIdx; i <= newRightMostIdx; i++) {
-        rightColorToCount[houses[i]]++;
-        if (rightColorToCount[houses[i]] == 1 && leftColorToCount[houses[i]] > 0) {
-            countLeftAndRight--;
-        }
-    }
-}
-
-bool haveSameHouses() {
-    return countLeftAndRight > 0;
-}
-
-bool updateSequence(int middleIdx) {
-    // Increment left bandit
-    int nextIdx = middleIdx;
-    for (int i = leftBandit.size() - 1; i >= 0; i--) {
-        int& curIdxIdx = leftBanditIdxIndexes[i];
-        int prevIdxIdx = curIdxIdx;
-        vector<int>& indexes = colorToIndexes[leftBandit[i]];
-
-        while (curIdxIdx < indexes.size() && indexes[curIdxIdx] <= nextIdx) {
-            curIdxIdx++;
+        // We stop if we didn't overshoot
+        while (curPos <= banditSize && sequence[curPos] >= sequence[curPos+1]) {
+            while (sequence[curPos] >= sequence[curPos+1] && sequence[curPos+1] < N)
+                sequence[curPos+1] = nextPtr[sequence[curPos+1]];
+            curPos++;
         }
 
-        if (curIdxIdx + 1 == indexes.size() && indexes[curIdxIdx - 1] <= nextIdx) {
-            return false;
-        } else {
-            curIdxIdx--;
-        }
-
-        if (DEBUG) {
-             cout << "---------\n";
-            cout << "leftBandit[i]: " << leftBandit[i] << "\n";
-            cout << "prevIdxIdx: " << prevIdxIdx << "\n";
-            cout << "curIdxIdx: " << curIdxIdx << "\n";
-            cout << "---------\n";
-        }
-       
-        // No change so no need to update
-        if (curIdxIdx == prevIdxIdx) {
-            break;
-        }
-    }
-
-    // Increment right bandit
-    nextIdx = middleIdx;
-    for (int i = leftBandit.size() - 1; i >= 0; i--) {
-        int& curIdxIdx = rightBanditIdxIndexes[i];
-        int prevIdxIdx = curIdxIdx;
-        vector<int>& indexes = colorToIndexes[rightBandit[i]];
-
-        while (curIdxIdx < indexes.size() && indexes[curIdxIdx] <= nextIdx) {
-            curIdxIdx++;
-        }
-
-        if (curIdxIdx = indexes.size()) {
-            return false;
-        }
-
-        if (DEBUG) {
-            cout << "---------\n";
-            cout << "rightBandit[i]: " << rightBandit[i] << "\n";
-            cout << "prevIdxIdx: " << prevIdxIdx << "\n";
-            cout << "curIdxIdx: " << curIdxIdx << "\n";
-            cout << "---------\n";
-        }
-
-        // No change so no need to update
-        if (curIdxIdx == prevIdxIdx) {
-            break;
-        }
-    }
-    return true;
-}
+        rangeEndingForIdx[banditNum][j] = sequence[banditSize]; // The last house of the sequence for the color 
+    }   
+} 
 
 int main() {
-    bool b = cinData();
-    if (!b) return 0;
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin >> N >> K;
 
-    vector<int> ans;
-    for (auto meet: colorToIndexes[meetColor]) {
-        bool b = updateSequence(meet);
-        
-        if (DEBUG) {
-            cout << "xxxxxxxxxxxxxxxxx\n";
-            cout << "meet: " << meet << "\n";
-            cout << "b: " << b << "\n";
-        }
-
-        if (!b) break;
-        updateCounts(
-            colorToIndexes[leftBandit[0]][leftBanditIdxIndexes[0]],
-            colorToIndexes[rightBandit[0]][rightBanditIdxIndexes[0]]
-        );
-
-        if (haveSameHouses()) {
-            ans.push_back(meet);
-        }
-
-        if (DEBUG) {
-            cout << "leftMostIdx: " << leftMostIdx << "\n";
-            cout << "rightMostIdx: " << rightMostIdx << "\n";
-            cout << "countLeftAndRight: " << countLeftAndRight << "\n";
-            cout << "xxxxxxxxxxxxxxxxx\n";
-        }
+    for (int i = 0; i < N; i++) {
+        cin >> houses[i];
+        houses[i]--;
     }
 
-    cout << ans.size() << "\n";
-    for (int i = 0; i < ans.size(); i++) {
-        cout << ans[i] + 1 << " ";
-    }
+    int size1, size2;
+    cin >> size1 >> size2;
+    setupBandit(size1, 0);
+    fill(result[0], result[0]+N, N);
+    for (int i = 0; i < N; i++)
+        result[0][houses[i]] = min(result[0][houses[i]], rangeEndingForIdx[0][i]);
+
+    reverse(houses, houses+N);
+    setupBandit(size2, 1);
+    for (int i = 0; i < N; i++)
+        result[1][houses[i]] = max(result[1][houses[i]], N-1-rangeEndingForIdx[1][i]);
+    reverse(houses, houses+N);
+
+    // Finding the biggest range of houses where we can search
+    pair<int, int> finalRes = {N, 0};
+    vector<int> solution;
+    for (int i = 0; i < K; i++)
+        if (result[0][i] <= result[1][i]) { // If we have a valid range
+            finalRes.first = min(finalRes.first, result[0][i]);
+            finalRes.second = max(finalRes.second, result[1][i]);
+        }
+
+    for (int j = finalRes.first; j <= finalRes.second; j++)
+        if (houses[j] == meetHouseColor)
+            solution.push_back(j);
+
+
+    cout << solution.size() << "\n";
+    for (auto u : solution)
+        cout << u+1 << " ";
     cout << "\n";
+
+    return 0;
 }
